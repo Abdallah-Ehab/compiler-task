754fc3493628ee49f2d355f4ddeec80a3a2db659
Abdallah Ehab <abdallahehab3710@gmail.com>
2024-11-05 19:59:28 +0200

    scanner first commit

--- code editor.py
+++ code editor.py
@@ -1,67 +1,87 @@
 import customtkinter as ctk
 import re
-from main import tokenize
+from main import Tokenizer
 
-def run_tokenizer():
+
+def get_tokens():
     text_content = text_widget.get("1.0", "end-1c")
-    tokens = tokenize(text_content)
+    tokenizer = Tokenizer(text_content)
+    return tokenizer.tokenize()
+
+def display_tokens():
+
+    tokens = get_tokens()
+    output.delete("1.0","end-1c")
+    for token in tokens:
+        output.insert("end",f"{token}\n")
     print(f"tokens:{tokens}")
 
-# Initialize customtkinter and set the appearance mode and theme
+
+
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("blue")
 
-# Create the main application window
+
 root = ctk.CTk()
 root.title("Simple Text Editor with Syntax Highlighting")
 root.geometry("500x400")
 
-# Define keywords and operators
-keywords = re.compile(r"\bif\b|\bwhile\b|\bfor\b|\bswitch\b|\bcase\b|\bint\b|\bdouble\b|\bprint\b")
-operator_pattern = re.compile(r"\+|\-|\*|\/|==|!=|<=|>=|<|>|&&|\|\|")
-identifier_pattern = re.compile(r"[\w_]+")
+
+keywords = re.compile(
+    r"\bauto\b|\bbreak\b|\bcase\b|\bchar\b|\bconst\b|\bcontinue\b|\bdefault\b|\bdefault\b|\bdouble\b|\belse\b|\benum\b|\bextern\b|\bfloat\b|\bfor\b|\bgoto\b|\bif\b|\binline\b|\bint\b|\blong\b|\bregister\b|\brestrict\b|\breturn\b|\bshort\b|\bsigned\b|\bsizeof\b|\bstatic\b|\bstruct\b|\bswitch\b|\btypedef\b|\bunion\b|\bunsigned\b|\bvoid\b|\bvolatile\b|\bwhile\b|\b_Bool\b|\b_Complex\b|\b_Imaginary\b")
+operator_pattern = re.compile(r"\+{1,2}|-{1,2}|={1,2}|&{1,2}|\|{1,2}|!|\/|%|\*|<<|>>|<|>|<=|>=|==|!=|\^|\?|\:")
+identifier_pattern = re.compile(r"[_a-zA-Z][_a-zA-Z0-9]*")
+comment_pattern = re.compile(r"//.*|/\*[\s\S]*?\*/$")
 
 
-# Define a function to apply syntax highlighting
+
 def highlight_syntax(event=None):
-    # Clear previous tags
-    text_widget.tag_remove("keyword", "1.0", "end")
-    text_widget.tag_remove("identifier","1.0","end")
-    text_widget.tag_remove("operator", "1.0", "end")
 
-    # Get the entire text
+    text_widget.tag_remove("keyword", "1.0", "end")
+    text_widget.tag_remove("identifier", "1.0", "end")
+    text_widget.tag_remove("operator", "1.0", "end")
+    text_widget.tag_remove("comment", "1.0", "end")
+
+
     text_content = text_widget.get("1.0", "end-1c")
 
-    for match in identifier_pattern.finditer(text_content):
-        start_idx = f"1.0 + {match.start()} chars"
-        end_idx = f"1.0 + {match.end()} chars"
-        text_widget.tag_add("identifier",start_idx,end_idx)
-    # Highlight keywords
+
     for match in keywords.finditer(text_content):
         start_idx = f"1.0 + {match.start()} chars"
         end_idx = f"1.0 + {match.end()} chars"
         text_widget.tag_add("keyword", start_idx, end_idx)
-    # Highlight operators
+
+    for match in identifier_pattern.finditer(text_content):
+        if keywords.fullmatch(match.group()) is None:
+            start_idx = f"1.0 + {match.start()} chars"
+            end_idx = f"1.0 + {match.end()} chars"
+            text_widget.tag_add("identifier", start_idx, end_idx)
+
+
     for match in operator_pattern.finditer(text_content):
         start_idx = f"1.0 + {match.start()} chars"
         end_idx = f"1.0 + {match.end()} chars"
         text_widget.tag_add("operator", start_idx, end_idx)
+    for match in comment_pattern.finditer(text_content):
+        start_idx = f"1.0 + {match.start()} chars"
+        end_idx = f"1.0 + {match.end()} chars"
+        text_widget.tag_add("comment", start_idx, end_idx)
 
-
-
-# Set up tags for styling
 text_widget = ctk.CTkTextbox(root, wrap="word", font=("Consolas", 12), undo=True)
 text_widget.pack(expand=True, fill="both", padx=10, pady=10)
-text_widget.tag_config("keyword", foreground="orange")
-text_widget.tag_config("operator", foreground="blue")
-text_widget.tag_config("identifier",foreground="orange")
+text_widget.tag_config("keyword", foreground="#ADD8E6")
+text_widget.tag_config("operator", foreground="#006400")
+text_widget.tag_config("identifier", foreground="#FF8C00")
+text_widget.tag_config("comment", foreground="#cdffcd")
 
-run_button = ctk.CTkButton(master= root,text="run",command= run_tokenizer).pack()
+run_button = ctk.CTkButton(master=root, text="run", command=display_tokens).pack()
 
-# Bind key release to the syntax highlighting function
+output = ctk.CTkTextbox(master=root, wrap="word", font=("Consolas", 12), undo=False)
+output.pack(expand=True, fill="both", padx=10, pady=10)
+
 text_widget.bind("<KeyRelease>", highlight_syntax)
 
-# Start the Tkinter main loop
+
 root.mainloop()
 
 
--- main.py
+++ main.py
@@ -1,49 +1,64 @@
 import re
 
-
 symbol_table = [
-    ("keyword", r"\bif\b|\bwhile\b|\bfor\b|\bswitch\b|\bcase\b|\bint\b|\bdouble\b|\bprint\b"),  
-    ("identifier", r"[\w_]+"),
-    ("String",r"\"[\w_&$-]*\""),
+    ("comment", r"//.*|/\*[\s\S]*?\*/$"),
+    ("keyword", r"\bauto\b|\bbreak\b|\bcase\b|\bchar\b|\bconst\b|\bcontinue\b|\bdefault\b|\bdefault\b|\bdouble\b|\belse\b|\benum\b|\bextern\b|\bfloat\b|\bfor\b|\bgoto\b|\bif\b|\binline\b|\bint\b|\blong\b|\bregister\b|\brestrict\b|\breturn\b|\bshort\b|\bsigned\b|\bsizeof\b|\bstatic\b|\bstruct\b|\bswitch\b|\btypedef\b|\bunion\b|\bunsigned\b|\bvoid\b|\bvolatile\b|\bwhile\b|\b_Bool\b|\b_Complex\b|\b_Imaginary\b"),
+    ("identifier", r"[_a-zA-Z][_a-zA-Z0-9]*"),
+    ("String", r"\"[\w_&$-]*\""),
     ("float", r"\d+\.\d+"),
     ("int", r"\d+"),
-    ("operator", r"\+{1,2}|-{1,2}|={1,2}|&{1,2}|\|{1,2}|!"),
-    ("special_character", r"[(){};,.\"\']"),
+    ("operator", r"\+{1,2}|-{1,2}|={1,2}|&{1,2}|\|{1,2}|!|\/|%|\*|<<|>>|<|>|<=|>=|==|!=|\^|\?|\:"),
+    ("special_character", r"[(){};,\.\"\']"),
+    ("new_line",r"\n"),
     ("white_space", r"\s+"),
-    ("tab",r"\t"),
-    ("comment",r"//|/\*.*?\*/$")
+    ("tab", r"\t"),
+
 ]
 
+class Token:
+    def __init__(self, lexeme, val, type, position):
+        self.lexeme = lexeme
+        self.val = val
+        self.type = type
+        self.position = position
 
-def make_pattern():
-    pattern = "|".join(pattern for _, pattern in symbol_table)
-    regex = re.compile(pattern)
-    return regex
+    def __repr__(self):
+        return f"Token(lexeme is {self.lexeme}, type={self.type}, val={self.val}, position={self.position})"
+
+class Tokenizer:
+    def __init__(self, text):
+        self.text = text
+        self.position = 0
+
+    def tokenize(self):
+        tokens = []
+        while self.position < len(self.text):
+            match = False
+            for type, pattern in symbol_table:
+                regex = re.compile(pattern)
+                matched = regex.match(self.text, self.position)
+                if matched:
+                    match = True
+                    lexeme = self.text[matched.start():matched.end()]
+                    if type == "float":
+                        token = Token(lexeme, float(lexeme), type, self.position)
+                    elif type == "int":
+                        token = Token(lexeme, int(lexeme), type, self.position)
+                    elif type in ["keyword", "identifier", "operator", "white_space", "comment","new_line","special_character"]:
+                        token = Token(lexeme, None, type, self.position)
+                    elif type == "string":
+                        token = Token(lexeme, lexeme, type, self.position)
+
+                    tokens.append(token)
+                    self.position = matched.end()
+                    break
+            if not match:
+                tokens.append(f"Unknown token at position {self.position} of value '{self.text[self.position]}'")
+                self.position += 1
+        return tokens
 
 
-def tokenize(text):
-    position = 0
-    tokens = []
+## to do parser: (to be continued) :
 
-    while position < len(text):
-        matched = False
 
-        
-        for name, pattern in symbol_table:
-            regex = re.compile(pattern)
-            matched_obj = regex.match(text, position)
-
-            if matched_obj:
-                matched = True
-                
-                if name not in ["white_space","tab"]:
-                    tokens.append((name, matched_obj.group(0)))
-                position = matched_obj.end()  
-                break  
-
-        if not matched:
-            print(f"unexpected word at position {position} of value '{text[position]}'")
-            position += 1 
-
-    return tokens
 
